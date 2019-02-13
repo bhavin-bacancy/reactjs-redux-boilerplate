2a230b229a9a120fa3ad9603e7197486011480c4
sildisha <ashvin.kumbhani@bacancytechnology.com>
2019-02-13 11:32:42 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+
+const title = 'My Minimal React Webpack Babel Setupt';
+
+ReactDOM.render(
+  <h1>{title}</h1>,
+  document.getElementById('app'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader', 'eslint-loader'],
+			},
+			{
+				test: /\.scss/,
+				exclude: /node_modules/,
+				loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' }),
+			},
+		],
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx'],
+	},
+	output: {
+		path: `${__dirname}/dist`,
+		publicPath: '/',  //`${__dirname}/dist`
+		filename: 'bundle.js',
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new ExtractTextPlugin('style.css'),
+		new HtmlWebpackPlugin({
+			template: "./index.html",
+      filename: "index.html"
+		}),
+	],
+	devServer: {
+		contentBase: './dist',
+		hot: true,
+	},
+};
